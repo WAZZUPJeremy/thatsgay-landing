cd02ed12b92cc117273042d1fde2fa9e9d9cadc6
WAZZUPJeremy <jeremy@wazzup.app>
2025-08-10 09:37:30 +0200

    Delete thatsgay-landing-next directory

--- thatsgay-landing-next/components/ui/card.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import clsx from 'clsx';
-
-export function Card({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={clsx('card', className)} {...props} />;
-}
-export function CardContent({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={clsx('p-6', className)} {...props} />;
-}
