d8aee08f6735382ddb87f3d138b23603e37818c6
WAZZUPJeremy <jeremy@wazzup.app>
2025-08-10 11:17:48 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,14 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
-      <body>{children}</body>
+      <body>{/* Plausible */}
+<script defer data-domain="https://thatsgay-landing.vercel.app/" src="https://plausible.io/js/script.js"></script>
+
+{/* TikTok Pixel (optionnel) */}
+<script dangerouslySetInnerHTML={{__html: `
+!function (w, d, t) { w.TiktokAnalyticsObject=t;var tt=w[t]=w[t]||[];tt.methods=["page","track"];tt.setAndDefer=function(t){tt[t]=function(){tt.push([t].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<tt.methods.length;i++)tt.setAndDefer(tt.methods[i]);tt.load=function(e){var i=d.createElement("script");i.async=!0,i.src="https://analytics.tiktok.com/i18n/pixel/events.js?sdkid="+e+"&lib=t";var n=d.getElementsByTagName("script")[0];n.parentNode.insertBefore(i,n)};tt.load("TIKTOK_PIXEL_ID");tt.page();}(window, document, 'ttq');`
+}}/>
+{children}</body>
     </html>
   );
 }
