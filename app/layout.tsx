2874701b3ee792f5542cce8c2e41a024a3a3590d
WAZZUPJeremy <jeremy@wazzup.app>
2025-08-10 10:26:40 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import "./globals.css";
 export const metadata = {
   title: "thatsgay? – Le débat le plus bête (et le plus drôle)",
   description: "Une app fun & second degré pour débattre entre potes : est‑ce que c'est gay ?",
